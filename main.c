6b864f324c1b54a4ccc4d826ff575faab486b791
ally bram <ab038817@ohio.edu>
2018-09-13 21:19:27 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    x = 6;
+    int y;
+    y = x + 3;
+    int z;
+    z = y + x;
+    printf("%d,%d,%d",x,y,z);
+
+
+    return 0;
+}
